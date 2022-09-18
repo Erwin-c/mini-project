c5912d8b6c96892d36d65ae2faba8911cbc1eea2
Erwin <274868686@qq.com>
2022-09-18 10:53:43 +0800

    ...

--- public/_public.cc
+++ public/_public.cc
@@ -635,3 +635,96 @@ CLogFile::~CLogFile() {
 
   // pthread_spin_destroy(&spin);
 }
+
+CSEM::CSEM() {
+  m_semid = -1;
+  m_sem_flg = SEM_UNDO;
+}
+
+bool CSEM::init(key_t key, unsigned short value, short sem_flg) {
+  if (m_semid != -1) {
+    return false;
+  }
+
+  m_sem_flg = sem_flg;
+
+  if ((m_semid = semget(key, 1, 0666)) == -1) {
+    if (errno == 2) {
+      if ((m_semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) == -1) {
+        if (errno != EEXIST) {
+          perror("init 1 semget()");
+          return false;
+        }
+        if ((m_semid = semget(key, 1, 0666)) == -1) {
+          perror("init 2 semget()");
+          return false;
+        }
+
+        return true;
+      }
+
+      union semun sem_union;
+      sem_union.val = value;
+      if (semctl(m_semid, 0, SETVAL, sem_union) < 0) {
+        perror("init semctl()");
+        return false;
+      }
+    } else {
+      perror("init 3 semget()");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool CSEM::P(short sem_op) {
+  if (m_semid == -1) {
+    return false;
+  }
+
+  struct sembuf sem_b;
+  sem_b.sem_num = 0;
+  sem_b.sem_op = sem_op;
+  sem_b.sem_flg = m_sem_flg;
+  if (semop(m_semid, &sem_b, 1) == -1) {
+    perror("P semop()");
+    return false;
+  }
+
+  return true;
+}
+
+bool CSEM::V(short sem_op) {
+  if (m_semid == -1) {
+    return false;
+  }
+
+  struct sembuf sem_b;
+  sem_b.sem_num = 0;
+  sem_b.sem_op = sem_op;
+  sem_b.sem_flg = m_sem_flg;
+  if (semop(m_semid, &sem_b, 1) == -1) {
+    perror("V semop()");
+    return false;
+  }
+
+  return true;
+}
+
+int CSEM::value() { return semctl(m_semid, 0, GETVAL); }
+
+bool CSEM::destroy() {
+  if (m_semid == -1) {
+    return false;
+  }
+
+  if (semctl(m_semid, 0, IPC_RMID) == -1) {
+    perror("destroy semctl()");
+    return false;
+  }
+
+  return true;
+}
+
+CSEM::~CSEM() {}
