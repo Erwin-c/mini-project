0f395623374bc8b580735a3e75e004d13adeea41
Erwin <erwin@localhost.localdomain>
2022-11-21 01:57:09 +0800

    ...

--- demo/db/updatetable.cc
+++ demo/db/updatetable.cc
@@ -1,15 +1,13 @@
 /*
- * inserttable.cc, 此程序演示开发框架操作 MySQL 数据库 (向表中插入5条记录).
+ * updatetable.cc, 此程序演示开发框架操作 MySQL 数据库 (修改表中的记录).
  *
  *  Author: Erwin
  */
 
-#include <unistd.h>
-
-#include "_mysql.h"  // 开发框架操作 MySQL 的头文件.
+#include "_mysql.h"  // 开发框架操作MySQL的头文件。
 
 int main() {
-  connection conn;  // 数据库连接类.
+  connection conn;  // 数据库连接类。
 
   // 登录数据库, 返回值: 0-成功; 其它是失败, 存放了 MySQL 的错误代码.
   // 失败代码在 conn.m_cda.rc 中, 失败描述在 conn.m_cda.message 中.
@@ -23,17 +21,15 @@ int main() {
     long id;         // 超女编号.
     char name[31];   // 超女姓名.
     double weight;   // 超女体重.
-    char btime[20];  // 报名时间.
+    char btime[20];  // 报名时间,
   } stgirls;
 
   sqlstatement stmt(&conn);  // 操作 SQL 语句的对象.
 
-  // 准备插入表的 SQL 语句.
+  // 准备修改表的 SQL 语句.
   stmt.prepare(
       "\
-    insert into girls(id,name,weight,btime) values(:1+1,:2,:3+45.35,str_to_date(:4,'%%Y-%%m-%%d %%H:%%i:%%s'))");
-  // insert into girls(id,name,weight,btime)
-  // values(?+1,?,?+45.35,to_date(?,'yyyy-mm-dd hh24:mi:ss'))");
+    update girls set name=:1,weight=:2,btime=str_to_date(:3,'%%Y-%%m-%%d %%H:%%i:%%s') where id=:4");
 
   /*
    * 注意事项:
@@ -52,21 +48,21 @@ int main() {
    *    SQL 语句的数据来自被绑定变量的值.
    */
 
-  stmt.bindin(1, &stgirls.id);
-  stmt.bindin(2, stgirls.name, 30);
-  stmt.bindin(3, &stgirls.weight);
-  stmt.bindin(4, stgirls.btime, 19);
+  stmt.bindin(1, stgirls.name, 30);
+  stmt.bindin(2, &stgirls.weight);
+  stmt.bindin(3, stgirls.btime, 19);
+  stmt.bindin(4, &stgirls.id);
 
-  // 模拟超女数据, 向表中插入 5 条测试数据.
-  for (int ii = 0; ii < 5; ++ii) {
+  // 模拟超女数据, 修改超女信息表中的全部记录.
+  for (int ii = 0; ii < 5; ii++) {
     // 结构体变量初始化.
     memset(&stgirls, 0, sizeof(stgirls));
 
     // 为结构体变量的成员赋值.
-    stgirls.id = ii;                                      // 超女编号.
-    sprintf(stgirls.name, "西施%05dgirl", ii + 1);        // 超女姓名.
-    stgirls.weight = ii;                                  // 超女体重.
-    sprintf(stgirls.btime, "2021-08-25 10:33:%02d", ii);  // 报名时间.
+    stgirls.id = ii + 1;                                  // 超女编号.
+    sprintf(stgirls.name, "貂蝉%05dgirl", ii + 1);        // 超女姓名.
+    stgirls.weight = ii + 48.39;                          // 超女体重.
+    sprintf(stgirls.btime, "2021-10-02 11:25:%02d", ii);  // 报名时间.
 
     // 执行 SQL 语句, 一定要判断返回值, 0-成功, 其它-失败.
     // 失败代码在 stmt.m_cda.rc 中, 失败描述在 stmt.m_cda.message 中.
@@ -77,10 +73,10 @@ int main() {
     }
 
     // stmt.m_cda.rpc 是本次执行 SQL 影响的记录数.
-    printf("成功插入了 %ld 条记录。\n", stmt.m_cda.rpc);
+    printf("成功修改了 %ld 条记录。\n", stmt.m_cda.rpc);
   }
 
-  printf("insert table girls ok.\n");
+  printf("update table girls ok.\n");
 
   // 提交数据库事务.
   conn.commit();
--- tools/c/procctl.cc
+++ tools/c/procctl.cc
@@ -1,5 +1,5 @@
 /*
- * procctl.cc
+ * procctl.cc, 本程序是服务程序的调度程序.
  *
  *  Author: Erwin
  */
@@ -7,35 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   if (argc < 3) {
     printf("Using: ./procctl timetvl program argv ...\n");
-    printf("Example: ../bin/procctl 5 /usr/bin/tar zcvf ./tmp.tgz ./\n");
+    printf(
+        "Example: ~/Coding/mini-project/tools/bin/procctl 5 "
+        "/usr/bin/ls\n\n");
 
-    printf("This program is scheduler\n");
+    printf("本程序是服务程序的调度程序, 周期性启动服务程序或 shell 脚本.\n");
+    printf(
+        "timetvl 运行周期, 单位: 秒. "
+        "被调度的程序运行结束后, 在 timetvl 秒后会被 procctl 重新启动.\n");
+    printf("program 被调度的程序名, 必须使用全路径.\n");
+    printf("argvs   被调度的程序的参数.\n");
 
-    printf("timetvl: time interval\n");
-    printf("program: need full path\n");
-    printf("argv: arguments\n");
-
-    printf("This program ignores all signals except for SIGKILL\n");
+    printf("注意, 本程序不会被 kill 杀死, 但可以用 kill -9 强行杀死.\n\n\n");
 
     return -1;
   }
 
+  // 关闭信号和IO, 本程序不希望被打扰.
   for (int ii = 0; ii < 64; ++ii) {
     signal(ii, SIG_IGN);
-    close(ii);
+    // close(ii);
   }
 
-  // Parent process exits
-  // Child process is hosted by systemd and runs in background
+  // 生成子进程, 父进程退出, 让程序运行在后台, 由系统 1 号进程托管.
   if (fork() != 0) {
     exit(0);
   }
 
+  // 启用 SIGCHLD 信号, 让父进程可以 wait 子进程退出的状态.
   signal(SIGCHLD, SIG_DFL);
 
   char *pargv[argc];
@@ -43,7 +46,7 @@ int main(int argc, char *argv[]) {
     pargv[ii - 2] = argv[ii];
   }
 
-  pargv[argc - 2] = NULL;
+  pargv[argc - 2] = nullptr;
 
   while (true) {
     if (fork() == 0) {
