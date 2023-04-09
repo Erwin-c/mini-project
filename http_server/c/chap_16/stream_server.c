a5534b9ee22e5ae21c513da9d4a12f8fd3c6df54
Erwin <erwin@localhost.localdomain>
2023-04-09 12:20:20 +0800

    ...

---
+++ http_server/c/chap_16/stream_server.c
@@ -0,0 +1,66 @@
+/*
+ * stream_server.c
+ *
+ *  Author: Erwin
+ */
+
+#include "lib/common.h"
+
+int count;
+
+char buf[128];
+
+void sig_int(int signo) {
+  printf("\nsigno: %d\n", signo);
+  printf("\nreceived %d datagrams\n", count);
+  exit(0);
+}
+
+int main(void) {
+  int listen_fd = 0, conn_fd = 0;
+  int on = 0;
+  socklen_t client_len = 0;
+  struct sockaddr_in server_addr = {0}, client_addr = {0};
+
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(SERV_PORT);
+
+  on = 1;
+  setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+  if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    error(1, errno, "bind failed");
+  }
+
+  if (listen(listen_fd, LISTENQ) == -1) {
+    error(1, errno, "listen failed");
+  }
+
+  signal(SIGPIPE, SIG_IGN);
+
+  if ((conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr,
+                        &client_len)) == -1) {
+    error(1, errno, "accept failed");
+  }
+
+  while (1) {
+    ssize_t read_rc = read_message(conn_fd, buf, sizeof(buf));
+    if (read_rc < 0) {
+      error(1, errno, "read message failed");
+    } else if (read_rc == 0) {
+      error(1, 0, "client closed");
+    }
+
+    buf[read_rc] = 0;
+
+    printf("received %ld bytes: %s\n", read_rc, buf);
+
+    ++count;
+  }
+
+  exit(0);
+}
--- http_server/c/lib/read.c
+++ http_server/c/lib/read.c
@@ -6,26 +6,58 @@
 
 #include "common.h"
 
-size_t readn(int fd, void* buffer, size_t size) {
+ssize_t readn(int fd, void* buffer, size_t size) {
   char* buffer_pointer = buffer;
   size_t length = size;
 
   while (length > 0) {
-    int result = read(fd, buffer_pointer, length);
+    ssize_t read_rc = read(fd, buffer_pointer, length);
 
-    if (result < 0) {
+    if (read_rc < 0) {
       if (errno == EINTR) {
         continue;  // 考虑非阻塞的情况, 这里需要再次调用 read().
       } else {
         return -1;
       }
-    } else if (result == 0) {
+    } else if (read_rc == 0) {
       break;  // EOF (End of File) 表示 Socket 关闭.
     }
 
-    length -= result;
-    buffer_pointer += result;
+    length -= read_rc;
+    buffer_pointer += read_rc;
   }
 
   return size - length;  // 返回的是实际读取的字节数.
+}
+
+ssize_t read_message(int fd, void* buffer, size_t length) {
+  u_int32_t msg_length = 0;
+  u_int32_t msg_type = 0;
+  ssize_t read_rc = 0;
+
+  // Retrieve the length of the record.
+  read_rc = readn(fd, &msg_length, sizeof(msg_length));
+  if (read_rc != sizeof(msg_length)) {
+    return read_rc < 0 ? -1 : 0;
+  }
+
+  msg_length = ntohl(msg_length);
+
+  read_rc = readn(fd, &msg_type, sizeof(msg_type));
+  if (read_rc != sizeof(msg_type)) {
+    return read_rc < 0 ? -1 : 0;
+  }
+
+  // 判断 buffer 是否可以容纳下数据.
+  if (msg_length > length) {
+    return -1;
+  }
+
+  // Retrieve the record itself.
+  read_rc = readn(fd, buffer, msg_length);
+  if (read_rc != msg_length) {
+    return read_rc < 0 ? -1 : 0;
+  }
+
+  return read_rc;
 }
